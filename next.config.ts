06c60412c45ec26bbd46a43cddca22b1da765d41
Kaiden Zapanta <kaidenzapanta@gmail.com>
2026-09-13 19:35:10 -0500

    Config fixes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    output: "export",
+  output: "export",
+  basePath: "/portfolio-next",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
